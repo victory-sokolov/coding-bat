04dc7d379470fbd0a5faff4b715340da5895a5ee
victory-sokolov <viktorsokolov.and@gmail.com>
2019-06-15 16:16:41 +0300

    added tests

--- java/src/warmup1/NearHundred.java
+++ java/src/warmup1/NearHundred.java
@@ -12,7 +12,7 @@ package warmup1;
 
 public class NearHundred {
 
-    public boolean nearHundred(int n) {
+    public static boolean nearHundred(int n) {
 
         if(Math.abs(100 - n) <= 10 || Math.abs(200 - n) <= 10) {
             return true;
